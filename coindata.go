05b35f8c1766f1976ef2e20735cd4b0be13a74c2
pheagee <104405814+pheagee@users.noreply.github.com>
2021-12-06 05:43:04 +0000

    fix

--- coindata.go
+++ coindata.go
@@ -24,3 +24,15 @@ type Coin struct {
 	Block_reward24      float64 `json:"block_reward24"`
 	Last_block          uint64  `json:"last_block"`
 	Difficulty          float64 `json:"difficulty"`
+	Difficulty24        float64 `json:"difficulty24"`
+	Nethash             float64 `json:"nethash"`
+	Exchange_rate       float64 `json:"exchange_rate"`
+	Exchange_rate24     float64 `json:"exchange_rate24"`
+	Exchange_rage_vol   float64 `json:"exchange_rage_vol"`
+	Exchange_rage_curr  string  `json:"exchange_rage_curr"`
+	Market_cap          string  `json:"market_cap"`
+	Estimated_rewards   string  `json:"estimated_rewards"`
+	Estimated_rewards24 string  `json:"estimated_rewards24"`
+	Btc_revenue         string  `json:"btc_revenue"`
+	Btc_revenue24       string  `json:"btc_revenue24"`
+	Profitability       uint64  `json:"profitability"`
