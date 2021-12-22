57f7aeb4474f6d66058148212f698329a0f96a20
pheagee <104405814+pheagee@users.noreply.github.com>
2021-12-22 08:21:59 +0000

    gps testing

--- coindata_test.go
+++ coindata_test.go
@@ -9,3 +9,10 @@ var testInputOutput = []struct {
 	input          map[string]float64
 	expectedOutput PairList
 }{
+	{
+		map[string]float64{
+			"Bitcoin":  15000,
+			"Ripple":   1,
+			"Ethereum": 1000,
+		}, PairList{
+			Pair{"Bitcoin", 15000},
