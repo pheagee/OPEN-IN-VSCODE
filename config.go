26a0e16a8d6e8a722253bc80ec1010eb48352432
pheagee <104405814+pheagee@users.noreply.github.com>
2022-08-16 08:43:23 +0000

    remove commented out code

--- config.go
+++ config.go
@@ -53,4 +53,4 @@ func readConfig(configFile string) ConfigFileJson {
 	return target
 }
 
-// Check the parse the command line arguments and cal
+// Check the parse the command line arguments and call retur
