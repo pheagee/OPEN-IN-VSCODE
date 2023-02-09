38bc87ad301eb9299475a6dc8d824e48479fa06e
pheagee <104405814+pheagee@users.noreply.github.com>
2023-02-09 12:27:26 +0000

    Closer

--- urlconstruct.go
+++ urlconstruct.go
@@ -5,3 +5,12 @@ import (
 	"bytes"
 	"strconv"
 	"strings"
+)
+
+const (
+	urlBase    string = "https://whattomine.com/coins.json?utf8=%E2%9C%93&"
+	trueString string = "true&"
+)
+
+// appends the strings adaptString and numberOfGPUs
+// additionally appends
