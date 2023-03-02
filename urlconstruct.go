c11dbbf8845802a423351b55f8d8f4c76f776589
pheagee <104405814+pheagee@users.noreply.github.com>
2023-03-02 11:05:04 +0000

    Add views

--- urlconstruct.go
+++ urlconstruct.go
@@ -21,3 +21,16 @@ func writeOneParameterQuery(buffer *bytes.Buffer, adaptString, numberOfGPUs stri
 	// Add adapt_MODEL=true& whenever there's > 0 cards for that model
 	if numberOfGPUs != "0" {
 		parts := strings.Split(adaptString, "_")
+		adapt_true := parts[0] + "_" + parts[2] + trueString
+		buffer.WriteString(adapt_true)
+	}
+}
+
+// Build the url with the query parameters for use in www.whattomine.com
+func constructUrlQuery(config ConfigFileJson, totalGPUsCharacteristics GPU) string {
+	var buffer bytes.Buffer
+	buffer.WriteString(urlBase)
+	writeOneParameterQuery(&buffer, "adapt_q_280x=", strconv.FormatUint(config.GPU.GPU280x, 10))
+	writeOneParameterQuery(&buffer, "adapt_q_380=", strconv.FormatUint(config.GPU.GPU380, 10))
+	writeOneParameterQuery(&buffer, "adapt_q_fury=", strconv.FormatUint(config.GPU.GPUFury, 10))
+	writeOneParameterQuery(&buffer, "adapt_q_470=", strconv.FormatUint(config.GPU.GPU470, 10))
