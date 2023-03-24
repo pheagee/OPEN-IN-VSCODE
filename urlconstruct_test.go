6d66fc00c24f08466a1d1d86e468f6cdff9b5e0a
pheagee <104405814+pheagee@users.noreply.github.com>
2023-03-24 22:52:26 +0000

    beautify

--- urlconstruct_test.go
+++ urlconstruct_test.go
@@ -7,3 +7,10 @@ import (
 )
 
 var writeOneParameterQueryTestInputOutput = []struct {
+	adaptString    string
+	numOfGPUs      string
+	expectedOutput string
+}{
+	{"adapt_q_280x=", "0", "adapt_q_280x=0&"},
+	{"adapt_q_280x=", "1", "adapt_q_280x=1&adapt_280x=true&"},
+}
