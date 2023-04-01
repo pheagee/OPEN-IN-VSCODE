620acc88f034ac49cf6e09ebb36f9de22021a2d1
pheagee <104405814+pheagee@users.noreply.github.com>
2023-04-01 22:15:02 +0000

    0.0.1

--- urlconstruct_test.go
+++ urlconstruct_test.go
@@ -14,3 +14,10 @@ var writeOneParameterQueryTestInputOutput = []struct {
 	{"adapt_q_280x=", "0", "adapt_q_280x=0&"},
 	{"adapt_q_280x=", "1", "adapt_q_280x=1&adapt_280x=true&"},
 }
+
+func TestWriteOneParameterQuery(t *testing.T) {
+	for _, vals := range writeOneParameterQueryTestInputOutput {
+		var buffer bytes.Buffer
+		writeOneParameterQuery(&buffer, vals.adaptString, vals.numOfGPUs)
+		actualResult := buffer.String()
+		expectedResult := vals.expectedOutput
